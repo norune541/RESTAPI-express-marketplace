219247fdca63b0d5a898241d34af6a93da691199
developmentKen <dev.ken1@proton.me>
2026-06-09 14:29:42 +0500

    feat: add prisma client

--- src/config/prisma.ts
+++ src/config/prisma.ts
@@ -1,5 +1,7 @@
 import { PrismaClient } from "@prisma/client";
 
-export const prisma = new PrismaClient({
+const prisma = new PrismaClient({
   log: process.env.NODE_ENV === "development" ? ["query", "error"] : ["error"],
 });
+
+export default prisma;
