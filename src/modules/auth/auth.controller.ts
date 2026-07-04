da4020cfe58e91c25094ff8ff4cc9976ca5ce94a
developmentKen <dev.ken1@proton.me>
2026-07-04 18:27:00 +0500

    feat: patch signup and login

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,9 +1,25 @@
 import type { Request, Response } from "express";
 import { CreateUserSchema } from "../users/create-user.dto";
 import { signup } from "./auth.service";
+import { LoginSchema } from "./login.dto";
+import { signin } from "./auth.service";
+import { json } from "../../shared/helpers/json";
 
-export const createUser = async (req: Request, res: Response) => {
+export const register = async (req: Request, res: Response) => {
   const parse = CreateUserSchema.parse(req.body);
-  const token = await signup(parse);
-  return res.status(201).json({ access_token: token });
+  const user = await signup(parse);
+  return res.status(201).send(json(user));
+};
+
+export const login = async (req: Request, res: Response) => {
+  const parse = LoginSchema.parse(req.body);
+  const { accessToken, refreshToken } = await signin(parse);
+
+  res.cookie("refreshToken", refreshToken, {
+    httpOnly: true,
+    secure: false, // true in production
+    sameSite: "strict",
+  });
+
+  return res.status(200).json({ accessToken });
 };
