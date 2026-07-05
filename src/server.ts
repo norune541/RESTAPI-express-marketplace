2f0463e0f5f721b0ac54f32813ab48a5757ce30d
developmentKen <dev.ken1@proton.me>
2026-07-05 19:54:22 +0500

    feat: add global bigint serializer

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,22 @@
 import "dotenv/config";
 import express from "express";
 import cors from "cors";
+import cookieParser from "cookie-parser";
 import { userRouter } from "./modules/users/user.routes";
 import { authRouter } from "./modules/auth/auth.routes";
 // import { advertRouter } from "./modules/adverts/advert.route";
-import { errorHandler } from "./middlewares/errorHandler";
+import { errorHandler } from "./shared/middlewares/errorHandler";
+
+(BigInt.prototype as any).toJSON = function () {
+  return this.toString();
+};
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
 // app.use("/api", advertRouter);
