804875a6c06269e7e8f6ecbe145f018a860a944c
developmentKen <dev.ken1@proton.me>
2026-07-07 21:52:05 +0500

    Revert "feat: add error message in zod string method"
    This reverts commit bcbd624bc24db1690df7c76db074eef956824d91.

--- src/modules/users/create-user.dto.ts
+++ src/modules/users/create-user.dto.ts
@@ -3,19 +3,10 @@ import { z } from "zod";
 const phoneRegex = /^\+?[1-9]\d{6,14}$/;
 
 export const CreateUserSchema = z.object({
-  name: z
-    .string("Name must be a string")
-    .trim()
-    .min(2, "Name must be atleast 2 characters"),
+  name: z.string().min(2, "Name should be atleast 2 characters"),
   email: z.email("Invalid email format"),
-  phone: z
-    .string("Phone must be a string")
-    .trim()
-    .regex(phoneRegex, "Invalid phone format"),
-  password: z
-    .string("Password must be a string")
-    .trim()
-    .min(8, "Password must be atleast 8 characters"),
+  phone: z.string().regex(phoneRegex, "Invalid phone format"),
+  password: z.string().min(8, "Password should be atleast 8 characters"),
 });
 
 export type CreateUserDto = z.infer<typeof CreateUserSchema>;
