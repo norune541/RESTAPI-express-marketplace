8b0a82cd76a17bc4a50ff8f24c34301404d1345b
developmentKen <dev.ken1@proton.me>
2026-07-11 17:44:50 +0500

    refactor(users): simplify getUser function by removing unused imports and directly using jwt payload

--- src/modules/users/user.controller.ts
+++ src/modules/users/user.controller.ts
@@ -1,12 +1,11 @@
 import type { Request, Response } from "express";
 import { ApiError } from "../../shared/errors/ApiError";
-import { toUserResponse } from "./response-user.dto";
-import * as userService from "../users/user.service";
+import { getCurrentUser } from "./user.service";
 
 export const getUser = async (req: Request, res: Response) => {
-  const user = await userService.getCurrentUser(Number(req.params.id));
+  const user = await getCurrentUser(req.user.id);
   if (!user) {
     throw new ApiError(`User ${req.params.id} not found`, 404);
   }
-  return res.status(200).json(toUserResponse(user));
+  return res.status(200).json(user);
 };
