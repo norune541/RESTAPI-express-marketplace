c59e4485a25865f0ef1ff797bc13c20c58b8c2fd
developmentKen <dev.ken1@proton.me>
2026-07-11 17:47:19 +0500

    refactor(auth): enhance type safety and simplify token validation in auth middleware

--- src/shared/middlewares/auth.ts
+++ src/shared/middlewares/auth.ts
@@ -1,29 +1,23 @@
 import jwt from "jsonwebtoken";
-import { User } from "../models/user.js";
-import { ApiError } from "../errors/ApiError.js";
+import { ApiError } from "../errors/ApiError";
+import type { Request, Response, NextFunction } from "express";
 
-export const auth = async (req, res, next) => {
+export const auth = async (req: Request, res: Response, next: NextFunction) => {
   try {
-    // Валидация токена
     const token = req.headers.authorization?.split(" ")[1];
     if (!token) {
-      return next(ApiError("Token not provided", 401));
-    }
-    const ok = jwt.verify(token, process.env.JWT_SECRET);
-
-    // Поиск токена пользователя
-    const user = await User.findToken(token);
-    if (user.token !== token) {
-      return next(ApiError("Unauthorized", 401));
+      throw new ApiError("No token provided", 401);
     }
 
-    // Продолжение flow
+    const decoded = jwt.verify(token, process.env.ACCESS_SECRET!);
+
     req.user = {
-      id: user.id,
-      role: user.role,
+      id: decoded.id,
+      role: decoded.role,
     };
+
     next();
   } catch (err) {
-    return next(ApiError("Unauthorized", 401));
+    throw new ApiError(err, 401);
   }
 };
