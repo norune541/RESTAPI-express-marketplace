7a2b225203871b7d0c61e69604535ad183bc91d3
developmentKen <dev.ken1@proton.me>
2026-07-11 17:50:51 +0500

    chore: commit obsolete files before deprecation

---
+++ src/modules/adverts/advert.routes.ts
@@ -0,0 +1,20 @@
+import { Router } from "express";
+import { auth } from "../../middlewares/auth.js";
+import { isAuthor } from "../../middlewares/isAuthor.js";
+import {
+  getAdvertsByUserId,
+  getCategories,
+  getAdvertByAdvertId,
+  updateStatus,
+  deleteAdvert,
+  updateAdvert,
+} from "../controllers/advert.js";
+
+export const advertRouter = Router();
+
+advertRouter.get("/user/adverts", auth, getAdvertsByUserId);
+advertRouter.get("/categories", getCategories);
+advertRouter.get("/adverts/:id", auth, getAdvertByAdvertId);
+advertRouter.delete("/adverts/:id", auth, isAuthor, deleteAdvert);
+advertRouter.post("/adverts/:id/update-status", auth, isAuthor, updateStatus);
+advertRouter.patch("/adverts/:id", auth, isAuthor, updateAdvert);
