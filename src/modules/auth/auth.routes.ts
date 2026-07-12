d1cad8dda0cde4f4c7460348b33aa6d292df8dba
developmentKen <dev.ken1@proton.me>
2026-07-12 17:12:52 +0500

    refactor(auth): update authentication routes to use token-based endpoints

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,10 +1,9 @@
 import { Router } from "express";
-import { register, login, logout, tokenRefresh } from "./auth.controller";
+import { login, logout, tokenRefresh } from "./auth.controller";
 import { auth } from "../../shared/middlewares/auth";
 
 export const authRouter = Router();
-// TODO: refactor routes to RESTful method
-authRouter.post("/register", register);
-authRouter.post("/login", login);
-authRouter.post("/refresh", tokenRefresh);
-authRouter.post("/logout", auth, logout);
+
+authRouter.post("/tokens", login);
+authRouter.post("/tokens/refresh", tokenRefresh);
+authRouter.delete("/tokens/current", auth, logout);
