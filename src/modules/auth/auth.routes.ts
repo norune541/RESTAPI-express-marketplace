8fed8dc3ded00aecc4943165855e19a2a831489c
developmentKen <dev.ken1@proton.me>
2026-07-12 15:50:05 +0500

    feat(auth): add logout route to authentication router

--- src/modules/auth/auth.routes.ts
+++ src/modules/auth/auth.routes.ts
@@ -1,8 +1,10 @@
 import { Router } from "express";
-import { register, login, tokenRefresh } from "./auth.controller";
+import { register, login, logout, tokenRefresh } from "./auth.controller";
+import { auth } from "../../shared/middlewares/auth";
 
 export const authRouter = Router();
-
+// TODO: refactor routes to RESTful method
 authRouter.post("/register", register);
 authRouter.post("/login", login);
 authRouter.post("/refresh", tokenRefresh);
+authRouter.post("/logout", auth, logout);
