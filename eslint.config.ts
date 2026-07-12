0f22ba60e7e8d80bc48a01467d46cb4471c2b565
developmentKen <dev.ken1@proton.me>
2026-07-12 14:07:27 +0500

    refactor(eslint): add custom rules for TypeScript linting

--- eslint.config.ts
+++ eslint.config.ts
@@ -7,4 +7,15 @@ import tseslint from "typescript-eslint";
 export default defineConfig({
   files: ["**/*.{js,ts}"],
   extends: [js.configs.recommended, tseslint.configs.recommended],
+  rules: {
+    "@typescript-eslint/no-explicit-any": "off",
+    "@typescript-eslint/no-unused-vars": [
+      "error",
+      {
+        argsIgnorePattern: "^_",
+        varsIgnorePattern: "^_",
+        caughtErrorsIgnorePattern: "^_",
+      },
+    ],
+  },
 });
