ae0d8b50d644da3dccf765744738c371fa5c2610
developmentKen <dev.ken1@proton.me>
2026-07-12 17:13:42 +0500

    feat(user): add user registration route to user router

--- src/modules/users/user.routes.ts
+++ src/modules/users/user.routes.ts
@@ -1,7 +1,9 @@
 import { Router } from "express";
 import { getUser } from "./user.controller";
 import { auth } from "../../shared/middlewares/auth";
+import { register } from "../auth/auth.controller";
 
 export const userRouter = Router();
 
 userRouter.get("/profile", auth, getUser);
+userRouter.post("/", register);
